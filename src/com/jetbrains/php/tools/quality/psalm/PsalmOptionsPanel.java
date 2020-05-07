9a072604615b96246b862e40cf89ca6b266492e7
Olga Strizhenko <olga.strizhenko@jetbrains.com>
2020-05-07 15:08:56 +0200

    quality tools: deduplicate
    GitOrigin-RevId: 6f78fd7fac9eb1ad15c0fe916731bd343fff4e91

--- src/com/jetbrains/php/tools/quality/psalm/PsalmOptionsPanel.java
+++ src/com/jetbrains/php/tools/quality/psalm/PsalmOptionsPanel.java
@@ -1,25 +1,15 @@
 package com.jetbrains.php.tools.quality.psalm;
 
-import com.intellij.ide.DataManager;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.project.ProjectManager;
-import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.ui.DocumentAdapter;
-import com.jetbrains.php.config.interpreters.PhpInterpreter;
-import com.jetbrains.php.config.interpreters.PhpInterpretersManagerImpl;
-import com.jetbrains.php.config.interpreters.PhpSdkAdditionalData;
 import com.jetbrains.php.config.interpreters.PhpTextFieldWithSdkBasedBrowse;
-import com.jetbrains.php.tools.quality.QualityToolValidationException;
-import com.jetbrains.php.tools.quality.phpcs.PhpCSConfiguration;
-import com.jetbrains.php.tools.quality.phpcs.PhpCSProjectConfiguration;
+import com.jetbrains.php.tools.quality.QualityToolsOptionsPanel;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 
-public class PsalmOptionsPanel {
+public class PsalmOptionsPanel extends QualityToolsOptionsPanel {
   private JPanel myOptionsPanel;
   private PhpTextFieldWithSdkBasedBrowse myConfigPathTextField;
   private final PsalmValidationInspection myInspection;
@@ -42,35 +32,11 @@ public class PsalmOptionsPanel {
   private void createUIComponents(){
   }
 
+  @Override
   public JPanel getOptionsPanel() {
     return myOptionsPanel;
   }
 
   public void init() {
-  }
-
-  @NotNull
-  private Project getCurrentProject() {
-    Project project = CommonDataKeys.PROJECT.getData(DataManager.getInstance().getDataContext(myOptionsPanel));
-    if (project == null) {
-      project = ProjectManager.getInstance().getDefaultProject();
-    }
-    return project;
-  }
-
-  @Nullable
-  private static PhpSdkAdditionalData getSdkAdditionalData(@NotNull Project project) {
-    try {
-      final PhpCSConfiguration configuration = PhpCSProjectConfiguration.getInstance(project).findSelectedConfiguration(project, false);
-      if (configuration == null || StringUtil.isEmpty(configuration.getInterpreterId())) {
-        return null;
-      }
-
-      final PhpInterpreter id = PhpInterpretersManagerImpl.getInstance(project).findInterpreterById(configuration.getInterpreterId());
-      return id != null ? id.getPhpSdkAdditionalData() : null;
-    }
-    catch (QualityToolValidationException e) {
-      return null;
-    }
   }
 }
