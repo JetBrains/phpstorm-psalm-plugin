aa00450995beda1c0a05919faf5104b4105dd023
Olga Strizhenko <olga.strizhenko@jetbrains.com>
2020-07-07 15:18:13 +0200

    WI-54175 psalm: detect config from composer.json
    GitOrigin-RevId: 281bd8c89553acf4a40d9cacff21856ad49c1255

--- src/com/jetbrains/php/tools/quality/psalm/PsalmComposerConfig.java
+++ src/com/jetbrains/php/tools/quality/psalm/PsalmComposerConfig.java
@@ -1,19 +1,26 @@
 package com.jetbrains.php.tools.quality.psalm;
 
+import com.google.gson.JsonElement;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Ref;
 import com.intellij.openapi.util.SystemInfo;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.jetbrains.php.composer.ComposerDataService;
 import com.jetbrains.php.composer.ComposerOpenSettingsProvider;
 import com.jetbrains.php.composer.actions.log.ComposerLogMessageBuilder;
 import com.jetbrains.php.tools.quality.QualityToolConfigurableList;
 import com.jetbrains.php.tools.quality.QualityToolConfigurationManager;
 import com.jetbrains.php.tools.quality.QualityToolType;
 import com.jetbrains.php.tools.quality.QualityToolsComposerConfig;
-import com.jetbrains.php.tools.quality.phpCSFixer.PhpCSFixerConfiguration;
-import com.jetbrains.php.tools.quality.phpCSFixer.PhpCSFixerQualityToolType;
 import com.jetbrains.php.ui.PhpUiUtil;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+import static com.intellij.openapi.util.text.StringUtil.*;
 
 public class PsalmComposerConfig extends QualityToolsComposerConfig<PsalmConfiguration, PsalmValidationInspection> implements 
                                          ComposerOpenSettingsProvider {
@@ -39,8 +46,39 @@ public class PsalmComposerConfig extends QualityToolsComposerConfig<PsalmConfigu
 
   @Override
   protected boolean applyRulesetFromComposer(@NotNull Project project, PsalmConfiguration configuration) {
+    final String configPath = ComposerDataService.getInstance(project).getConfigPath();
+    final VirtualFile config = LocalFileSystem.getInstance().refreshAndFindFileByPath(configPath);
+    if (config == null) return false;
+
+    final String ruleset = getRuleset(config);
+    if (ruleset == null) return false;
+    final VirtualFile customRulesetFile = detectCustomRulesetFile(config.getParent(), ruleset);
+    if (customRulesetFile != null) {
+      return modifyRulesetInspectionSetting(project, tool -> applyRuleset(tool, customRulesetFile.getPath()));
+    }
     return false;
   }
+
+  @Override
+  protected void checkComposerScriptsLeaves(JsonElement element, Ref<String> result) {
+    final String string = element.getAsString();
+    if (string != null && string.contains("psalm")) {
+      final List<String> split = split(string, " ");
+      for (String arg: split) {
+        final String prefix = "--config=";
+        if (startsWith(arg, prefix)) {
+          result.set(trimStart(arg, prefix));
+          return;
+        }
+        final int index = split.indexOf(arg);
+        if (StringUtil.equals(arg, "-c") && index < split.size() - 1) {
+          result.set(split.get(index + 1));
+          return;
+        }
+      }
+    }
+  }
+  
   @Override
   protected boolean applyRulesetFromRoot(@NotNull Project project) {
     VirtualFile customRulesetFile = detectCustomRulesetFile(project.getBaseDir(), PSALM_XML);
