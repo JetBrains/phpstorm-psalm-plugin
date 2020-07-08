8dc4af5f471e3590f5b5840c35e75cc59d1d21fd
Kirill Smelov <kirill.smelov@jetbrains.com>
2020-07-08 14:00:59 +0300

    PHP Psalm refactoring: Make PsalmParamTypeProvider logic PSI-based
    After #13 changes doc identifiers with "-" are parsed as single PhpDocType, so it's possible to replace string based workaround with more readable PSI-based logic
    
    GitOrigin-RevId: 1d0657936bdd2125a5859c58878521ae28bce111

--- src/com/jetbrains/php/psalm/types/PsalmParamTypeProvider.java
+++ src/com/jetbrains/php/psalm/types/PsalmParamTypeProvider.java
@@ -4,8 +4,12 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiElement;
-import com.intellij.util.text.StringTokenizer;
+import com.intellij.util.ObjectUtils;
+import com.jetbrains.php.lang.documentation.phpdoc.lexer.PhpDocTokenTypes;
+import com.jetbrains.php.lang.documentation.phpdoc.parser.PhpDocElementTypes;
 import com.jetbrains.php.lang.documentation.phpdoc.psi.PhpDocType;
+import com.jetbrains.php.lang.documentation.phpdoc.psi.impl.PhpDocTypeImpl;
+import com.jetbrains.php.lang.psi.PhpPsiUtil;
 import com.jetbrains.php.lang.psi.elements.PhpNamedElement;
 import com.jetbrains.php.lang.psi.resolve.types.PhpType;
 import com.jetbrains.php.lang.psi.resolve.types.PhpTypeProvider4;
@@ -14,6 +18,10 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
 import java.util.Set;
+import java.util.stream.Stream;
+
+import static com.jetbrains.php.lang.documentation.phpdoc.lexer.PhpDocTokenTypes.*;
+import static com.jetbrains.php.lang.psi.PhpPsiUtil.isOfType;
 
 public class PsalmParamTypeProvider implements PhpTypeProvider4 {
 
@@ -24,41 +32,17 @@ public class PsalmParamTypeProvider implements PhpTypeProvider4 {
 
   @Override
   public @Nullable PhpType getType(PsiElement element) {
-    return element instanceof PhpDocType ? parsePsalmType(element.getText()) : null;
+    return element instanceof PhpDocType ? parsePsalmType((PhpDocType)element) : null;
   }
 
-  public static PhpType parsePsalmType(String type) {
-    PsalmType psalmType = parsePsalmType(new PsalmArrayTokenizer(type), 0);
+  private static PhpType parsePsalmType(@NotNull PhpDocType type) {
+    PsalmType psalmType = parsePsalmType(type, 0);
     return psalmType.parsed() ? psalmType.getType() : null;
   }
 
-  private static class PsalmArrayTokenizer extends StringTokenizer {
-    private String myCurrentToken = null;
-
-    private PsalmArrayTokenizer(@NotNull String str) {
-      super(str, " \t,><", true);
-    }
-
-    private  @Nullable String next() {
-      if (!hasMoreTokens()) {
-        return null;
-      }
-      return myCurrentToken = getNextTokenSkippingWhitespace();
-    }
-
-
-    @Nullable
-    private String getNextTokenSkippingWhitespace() {
-      String nextToken = nextToken();
-      while (nextToken.trim().isEmpty()) {
-        if (!hasMoreTokens()) return null;
-        nextToken = nextToken();
-      }
-      return nextToken;
-    }
-  }
-
   private static class PsalmType extends Pair<PhpType, Boolean> {
+    private static final PsalmType NOT_PARSED = new PsalmType(PhpType.EMPTY, false);
+
     private PsalmType(PhpType type, boolean parsedArray) {
       super(type, parsedArray);
     }
@@ -72,19 +56,31 @@ public class PsalmParamTypeProvider implements PhpTypeProvider4 {
     }
   }
 
-  public static PsalmType parsePsalmType(PsalmArrayTokenizer tokenizer, int depth) {
-    //identifier|array<template [, template]>
-    String identifier = StringUtil.notNullize(tokenizer.next());
-    String openingTag = tokenizer.next();
-    if (!StringUtil.equalsIgnoreCase(openingTag, "<")) {
-      PhpType type = new PhpType();
-      StringUtil.split(identifier, "|").forEach(type::add);
-      return new PsalmType(type.pluralise(depth), false);
+  public static PsalmType parsePsalmType(@NotNull PhpDocType docType, int depth) {
+    String name = StringUtil.notNullize(docType.getName());
+    if (!"array".equalsIgnoreCase(name) && !"list".equalsIgnoreCase(name)) {
+      return new PsalmType(PhpDocTypeImpl.getLocalType(docType).pluralise(depth), false);
     }
-    PhpType key = parsePsalmType(tokenizer, depth + 1).getType();
-    PhpType value = StringUtil.equalsIgnoreCase(tokenizer.myCurrentToken, ",") ? parsePsalmType(tokenizer, depth + 1).getType() : key;
-    tokenizer.next(); // ">"
-    return new PsalmType(value, true);
+    PhpType typeFromDocTypeValues = valueDocTypeTags(docType)
+      .map(t -> parsePsalmType(t, depth + 1))
+      .map(PsalmType::getType)
+      .reduce(new PhpType(), PhpType::add);
+    return !typeFromDocTypeValues.isEmpty() ? new PsalmType(typeFromDocTypeValues, true) : PsalmType.NOT_PARSED;
+  }
+
+  @NotNull
+  public static Stream<PhpDocType> valueDocTypeTags(@NotNull PhpDocType docType) {
+    PsiElement attributes = PhpPsiUtil.getChildOfType(docType, PhpDocElementTypes.phpDocAttributeList);
+    if (attributes == null) {
+      return Stream.empty();
+    }
+    PsiElement attributesFirstChild = attributes.getFirstChild();
+    if (!(isOfType(attributesFirstChild, DOC_LAB) && isOfType(attributes.getLastChild(), DOC_RAB))) {
+      return Stream.empty();
+    }
+    PsiElement elementToFindCommentsAfter = ObjectUtils.notNull(PhpPsiUtil.getChildOfType(attributes, DOC_COMMA), attributesFirstChild);
+    return PhpPsiUtil.<PhpDocType>getChildren(attributes, PhpDocType.class::isInstance).stream()
+      .filter(t -> t.getTextOffset() > elementToFindCommentsAfter.getTextOffset());
   }
 
   @Override
