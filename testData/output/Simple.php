3eb4bb7005bf83ada8d7938e503e896fa1f1581b
Olga Strizhenko <olga.strizhenko@jetbrains.com>
2020-09-28 18:56:46 +0200

    Highlighting miss the first character and is not limited to the actual error #51 - fix tests
    GitOrigin-RevId: 9d4a4558a64ce2898589d7d4fcbb4e6cad6dc8be

--- testData/output/Simple.php
+++ testData/output/Simple.php
@@ -1,17 +1,17 @@
 <?php
-$<warning descr="psalm: UnusedVariable: Variable $fff is never referenced">fff = "";</warning>
+<warning descr="psalm: UnusedVariable: Variable $fff is never referenced">$fff = "";</warning>
 /**
- * @return a<warning descr="psalm: InvalidReturnType: The declared return type 'array<array-key, string>' for takesAnInt is incorrect, got 'array{int, string(ho)}'">rray<string></warning>
+ * @return <warning descr="psalm: InvalidReturnType: The declared return type 'array<array-key, string>' for takesAnInt is incorrect, got 'array{int, string(ho)}'">array<string></warning>
  * halifax templated
  */
 function takesAnInt(int $i) {
-    return [<warning descr="psalm: InvalidReturnStatement: The inferred type 'array{int, string(ho)}' does not match the declared return type 'array<array-key, string>' for takesAnInt">$i, "ho"];</warning>
+    return <warning descr="psalm: InvalidReturnStatement: The inferred type 'array{int, string(ho)}' does not match the declared return type 'array<array-key, string>' for takesAnInt">[$i, "ho"];</warning>
 }
 
 $data = ["some text", 5];
-takesAnInt($<warning descr="psalm: InvalidScalarArgument: Argument 1 of takesAnInt expects int, string(some text) provided">data[0]);</warning>
+takesAnInt(<warning descr="psalm: InvalidScalarArgument: Argument 1 of takesAnInt expects int, string(some text) provided">$data[0]);</warning>
 /**
- * @psalm-suppress I<warning descr="psalm: UnusedPsalmSuppress: This suppression is never used">nvalidReturnType</warning>
+ * @psalm-suppress <warning descr="psalm: UnusedPsalmSuppress: This suppression is never used">InvalidReturnType</warning>
  */
 $condition = rand(0, 5);
 if ($condition) {
