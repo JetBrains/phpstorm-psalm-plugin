4de5596c3bfbbe3d618d33cb24cb3ce5618114c7
Kirill Smelov <kirill.smelov@jetbrains.com>
2020-10-06 10:29:56 +0300

    PHP Psalm Try to fetch custom type doc tag from containing class doc comment also
    fixes #57
    
    GitOrigin-RevId: 3c5ce2f801aac089876a6c2422620fe890a4015c

--- testData/codeInsight/inspections/UndefinedClassTemplate.php
+++ testData/codeInsight/inspections/UndefinedClassTemplate.php
@@ -13,3 +13,22 @@
 function makeArray($t) {
     return [$t];
 }
+
+
+/**
+ * @template T
+ */
+class MyContainer {
+    /** @var T */
+    private $value;
+
+    /** @param T $value */
+    public function __construct($value) {
+        $this->value = $value;
+    }
+
+    /** @return T */
+    public function getValue() {
+        return $this->value;
+    }
+}
