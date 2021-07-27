a5d4505aee4bd9ea773ee06aeb752b50df9eb092
Kirill Smelov <kirill.smelov@jetbrains.com>
2021-07-27 10:16:58 +0300

    WI-61438 Expand extended templates over implemented classes with fallthrough templates
    (cherry picked from commit e6c2201b063d221bf4cfa6e7cde1139dc20ca689)
    
    GitOrigin-RevId: a70c1100ed411ff041f7e6735f57440579387a4a

---
+++ testData/codeInsight/typeInference/ExtendedClassesWithFallthroughTemplates.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @template T
+ */
+class A {
+    /**
+     * @return T
+     */
+    function get() {}
+}
+
+/**
+ * @return A<User>
+ */
+function f() {}
+<type value="mixed|User">f()->get()</type>;
+
+/**
+ * @template T
+ * @template-extends A<T>
+ */
+class B extends A {
+
+}
+
+/**
+ * @return B<User>
+ */
+function f1() {}
+<type value="T|User">f1()->get()</type>;
