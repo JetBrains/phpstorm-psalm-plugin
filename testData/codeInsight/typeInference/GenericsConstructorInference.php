d2fbda61dd001f957f2fe86c7017db81f0d401db
Kirill Smelov <kirill.smelov@jetbrains.com>
2021-08-02 13:17:17 +0300

    WI-60895 Infer generic type from constructor
    GitOrigin-RevId: d3e1346c909c42bb8d6dcde99a8c80fdc1cfd95e

---
+++ testData/codeInsight/typeInference/GenericsConstructorInference.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * @template T
+ * @template T1
+ */
+class Collection{
+    /**
+     * @param T $item
+     * @param int $pos
+     * @param T1 $second
+     */
+    public function __construct($item, $pos, $second)
+    {
+    }
+
+    /**
+     * @return T
+     */
+    public function first()
+    {
+    }
+
+    /**
+     * @return T1
+     */
+    public function second(){
+    }
+}
+
+
+
+class ItemA {
+}
+class ItemB {
+}
+
+function a(ItemA $a, ItemB $b){
+    $collection = new Collection($a, 0, $b);
+    <type value="ItemA|mixed">$b</type> = $collection->first();
+    <type value="mixed|ItemB">$b</type> = $collection->second();
+
+    $collection = new Collection(new ItemB(), 0, new ItemA());
+    <type value="mixed|ItemB">$b</type> = $collection->first();
+    <type value="ItemA|mixed">$b</type> = $collection->second();
+}
