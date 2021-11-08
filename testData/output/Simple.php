9a9bfe992ee9eda9af0bc10e2c2961b3228e4342
Alexey Kudravtsev <cdr@intellij.com>
2021-11-08 13:21:07 +0100

    fix test data to make it consistent with plugin.xml
    GitOrigin-RevId: b9d6dd18c77a4338e33f7711512373900336a893

--- testData/output/Simple.php
+++ testData/output/Simple.php
@@ -1,18 +1,18 @@
 <?php
-<warning descr="psalm: UnusedVariable: Variable $fff is never referenced">$fff = "";</warning>
+<weak_warning descr="psalm: UnusedVariable: Variable $fff is never referenced">$fff = "";</weak_warning>
 /**
- * @return <warning descr="psalm: InvalidReturnType: The declared return type 'array<array-key, string>' for takesAnInt is incorrect, got 'array{int, string(ho)}'">array<string></warning>
+ * @return <weak_warning descr="psalm: InvalidReturnType: The declared return type 'array<array-key, string>' for takesAnInt is incorrect, got 'array{int, string(ho)}'">array<string></weak_warning>
  * halifax templated
  */
 function takesAnInt(int $i) {
-    return <warning descr="psalm: InvalidReturnStatement: The inferred type 'array{int, string(ho)}' does not match the declared return type 'array<array-key, string>' for takesAnInt">[$i, "ho"];</warning>
+    return <weak_warning descr="psalm: InvalidReturnStatement: The inferred type 'array{int, string(ho)}' does not match the declared return type 'array<array-key, string>' for takesAnInt">[$i, "ho"];</weak_warning>
 }
 
 $data = ["some text", 5];
-takesAnInt(<warning descr="psalm: InvalidScalarArgument: Argument 1 of takesAnInt expects int, string(some text) provided">$data[0]);</warning>
+takesAnInt(<weak_warning descr="psalm: InvalidScalarArgument: Argument 1 of takesAnInt expects int, string(some text) provided">$data[0]);</weak_warning>
 /**
- * @psalm-suppress <warning descr="psalm: UnusedPsalmSuppress: This suppression is never used">InvalidReturnType</warning>
+ * @psalm-suppress <weak_warning descr="psalm: UnusedPsalmSuppress: This suppression is never used">InvalidReturnType</weak_warning>
  */
 $condition = rand(0, 5);
 if ($condition) {
-<warning descr="psalm: TypeDoesNotContainType: Found a contradiction when evaluating $condition and trying to reconcile type 'int(0)' to !falsy">} elseif ($condition) {}</warning>
+<weak_warning descr="psalm: TypeDoesNotContainType: Found a contradiction when evaluating $condition and trying to reconcile type 'int(0)' to !falsy">} elseif ($condition) {}</weak_warning>
