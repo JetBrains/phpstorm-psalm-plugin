956a56a5cafeb4c7f8a2318bd047cd76152866fa
Aleksandr.Slapoguzov <aleksandr.slapoguzov@jetbrains.com>
2022-03-25 09:18:23 +0100

    WI-60661: consider array shapes as extending of original function signature
    (cherry picked from commit 06e604e99bf6e18a5f69c43d9943a69754993b5a)
    
    IJ-CR-22582
    
    GitOrigin-RevId: ebb7c7248e499d5f086e4807ba358ae5f0f3ef3a

---
+++ testData/codeInsight/inspections/ArrayShapeExtendsFunctionSignature.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @return array{Exception,int}
+ */
+function f1(Exception $type): array
+{
+}
+
+/**
+ * @return array{Exception,int}
+ */
+function f2(): int
+{
+}
+
+/**
+ * @return array{Exception,int}|int
+ */
+function f3()
+{
+}
+
+/**
+ * @param $a array{Exception,int}
+ */
+function f4(array $a, $b): int
+{
+}
