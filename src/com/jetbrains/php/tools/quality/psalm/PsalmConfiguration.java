473c75ed4b88d7d861c4ae9b0a41909f586a8cf7
Olga Strizhenko <olga.strizhenko@jetbrains.com>
2022-05-12 13:34:12 +0200

    WI-66643 Rename "Local" configuration for Quality Tools to "System PHP"
    GitOrigin-RevId: 05260492620d77b7a43f9148ffc4c42cd1d77a5e

--- src/com/jetbrains/php/tools/quality/psalm/PsalmConfiguration.java
+++ src/com/jetbrains/php/tools/quality/psalm/PsalmConfiguration.java
@@ -63,12 +63,6 @@ public class PsalmConfiguration implements QualityToolConfiguration {
 
   @Override
   @NotNull
-  public @NlsContexts.Label String getPresentableName(@Nullable Project project) {
-    return getId();
-  }
-
-  @Override
-  @NotNull
   public @Nls String getId() {
     return PsalmBundle.message("local");
   }
@@ -99,10 +93,10 @@ public class PsalmConfiguration implements QualityToolConfiguration {
       return 1;
     }
 
-    if (StringUtil.equals(getPresentableName(null), PsalmBundle.message("local"))) {
+    if (StringUtil.equals(getPresentableName(null), PsalmBundle.message("label.system.php"))) {
       return -1;
     }
-    else if (StringUtil.equals(o.getPresentableName(null), PsalmBundle.message("local"))) {
+    else if (StringUtil.equals(o.getPresentableName(null), PsalmBundle.message("label.system.php"))) {
       return 1;
     }
     return StringUtil.compare(getPresentableName(null), o.getPresentableName(null), false);
