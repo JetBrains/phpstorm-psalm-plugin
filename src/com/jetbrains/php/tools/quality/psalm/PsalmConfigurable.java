a5ca380cbbd0527c59b1f039cbfb130c3d80a702
Olga Strizhenko <olga.strizhenko@jetbrains.com>
2022-06-08 15:56:26 +0200

    WI-44541 Add an option to apply PHP CS Fixer rules to Reformat File Dialog -
    move options from inspection to tool settings
    
    GitOrigin-RevId: 2f3a01a8f14bc54c3e9f7b0a5bc8fdbe87176fc1

--- src/com/jetbrains/php/tools/quality/psalm/PsalmConfigurable.java
+++ src/com/jetbrains/php/tools/quality/psalm/PsalmConfigurable.java
@@ -4,12 +4,18 @@ import com.intellij.openapi.options.Configurable;
 import com.intellij.openapi.project.Project;
 import com.jetbrains.php.tools.quality.QualityToolProjectConfigurableForm;
 import com.jetbrains.php.tools.quality.QualityToolType;
+import com.jetbrains.php.tools.quality.QualityToolsOptionsPanel;
 import org.jetbrains.annotations.NotNull;
 
 public class PsalmConfigurable extends QualityToolProjectConfigurableForm implements Configurable.NoScroll {
 
   public PsalmConfigurable(@NotNull Project project) {
     super(project);
+  }
+
+  @Override
+  protected QualityToolsOptionsPanel getQualityToolOptionPanel() {
+    return new PsalmOptionsPanel(new PsalmGlobalInspection());
   }
 
   @Override
