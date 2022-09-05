a7eff52816f28645932f0d0af5f9ff6ebfcd7e32
Kirill Smelov <kirill.smelov@jetbrains.com>
2022-09-05 19:15:27 +0200

    WI-68434 Parsing of template for phpstan/psalm tags
    GitOrigin-RevId: b908cfd14f5a71e3588eeace43df3810b0b03097

---
+++ src/com/jetbrains/php/psalm/lang/documentation/parser/PsalmDocTemplateParser.java
@@ -0,0 +1,6 @@
+package com.jetbrains.php.psalm.lang.documentation.parser;
+
+import com.jetbrains.php.lang.documentation.phpdoc.parser.tags.PhpDocTemplateTagParser;
+
+public class PsalmDocTemplateParser extends PhpDocTemplateTagParser {
+}
