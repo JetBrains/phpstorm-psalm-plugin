1c37383998cbbdf32cd31626c14ec35a34a5e156
Kirill Smelov <kirill.smelov@jetbrains.com>
2023-02-20 11:07:49 +0100

    WI-70991 Parse double quoted doc types
    (cherry picked from commit e56b95c8b9eb3ee17f1fde2d1e2522418a40ed05)
    
    IJ-CR-103257
    
    GitOrigin-RevId: c336ccb759706168fbe2bb0ddcd4fd3379e69a73

--- testData/codeInsight/inspections/LiteralTypeDuplicate.php
+++ testData/codeInsight/inspections/LiteralTypeDuplicate.php
@@ -1,6 +1,8 @@
 <?php
 
 /** @var 'one'|'two' $param */
+/** @var "one"|"two" $param */
+/** @var "two"|<weak_warning descr="Duplicate type '\"two\"'">"two"</weak_warning> $param */
 /** @var 'one'|<weak_warning descr="Duplicate type ''one''">'one'</weak_warning> $param */
 /** @var ''|<weak_warning descr="Duplicate type ''''">''</weak_warning> $param */
 /** @var ' '|'' $param */
--- testData/codeInsight/typeInference/LiteralType.php
+++ testData/codeInsight/typeInference/LiteralType.php
@@ -4,3 +4,8 @@
 function foo() {}
 
 <type value="string">foo()</type>;
+
+/** @return "one"|"two" */
+function foo1() {}
+
+<type value="string">foo1()</type>;
