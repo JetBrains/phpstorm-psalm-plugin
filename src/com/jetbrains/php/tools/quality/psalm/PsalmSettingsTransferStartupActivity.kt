4dd02cd2d3d412938e20e8ca41bccc448db1f575
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2024-01-08 19:54:34 +0100

    [psalm] Convert PsalmSettingsTransferStartupActivity to new ProjectActivity API
    GitOrigin-RevId: e7aafa00b5ca6af021ca2aac9d266707507ad608

--- src/com/jetbrains/php/tools/quality/psalm/PsalmSettingsTransferStartupActivity.kt
+++ src/com/jetbrains/php/tools/quality/psalm/PsalmSettingsTransferStartupActivity.kt
@@ -1,34 +1,29 @@
-package com.jetbrains.php.tools.quality.psalm;
+package com.jetbrains.php.tools.quality.psalm
 
-import com.intellij.openapi.application.Application;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.startup.StartupActivity;
-import com.intellij.profile.codeInspection.InspectionProfileManager;
-import com.intellij.util.PlatformUtils;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.ProjectActivity
+import com.intellij.profile.codeInspection.InspectionProfileManager
+import com.intellij.util.PlatformUtils
 
-public class PsalmSettingsTransferStartupActivity implements StartupActivity {
-  @Override
-  public void runActivity(@NotNull Project project) {
-    if (project.isDefault()) {
-      return;
+class PsalmSettingsTransferStartupActivity : ProjectActivity {
+  override suspend fun execute(project: Project) {
+    if (project.isDefault) {
+      return
     }
-    Application app = ApplicationManager.getApplication();
-    if (app.isUnitTestMode() || app.isHeadlessEnvironment() || !PlatformUtils.isPhpStorm()) {
-      return;
+    val app = ApplicationManager.getApplication()
+    if (app.isUnitTestMode || app.isHeadlessEnvironment || !PlatformUtils.isPhpStorm()) {
+      return
     }
 
-    PsalmGlobalInspection tool =
-      (PsalmGlobalInspection)PsalmQualityToolType.INSTANCE.getGlobalTool(project,
-                                                                         InspectionProfileManager.getInstance(project).getCurrentProfile());
-    PsalmOptionsConfiguration instance = PsalmOptionsConfiguration.getInstance(project);
-    if (tool != null && !instance.isTransferred()) {
-      instance.setConfig(tool.config);
-      instance.setFindUnusedCode(tool.findUnusedCode);
-      instance.setShowInfo(tool.showInfo);
-      instance.setFindUnusedSuppress(tool.findUnusedSuppress);
-      instance.setTransferred(true);
+    val tool = PsalmQualityToolType.INSTANCE.getGlobalTool(project, InspectionProfileManager.getInstance(project).currentProfile) as? PsalmGlobalInspection
+    val instance = PsalmOptionsConfiguration.getInstance(project)
+    if (tool != null && !instance.isTransferred) {
+      instance.config = tool.config
+      instance.isFindUnusedCode = tool.findUnusedCode
+      instance.isShowInfo = tool.showInfo
+      instance.isFindUnusedSuppress = tool.findUnusedSuppress
+      instance.isTransferred = true
     }
   }
 }
