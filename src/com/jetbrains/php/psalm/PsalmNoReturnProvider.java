8b6c9f30ac529e8919ddb10e4a4ae115a5596c87
Maria Filippova <maria.filippova@jetbrains.com>
2024-03-01 11:02:12 +0100

    WI-75735 PHP Performance: Don't resolve function to check `void`/`never`
    This commit consists of changed commits a16bf95d, 06096a5d (lately `getType()` was checked during stubs' building and function names with types `void`/`never` were added to indexes based on this check, now because we decided to get rid of computing `getType()` during stub building, we can check only `declaredType`, so additional indexes for functions that are not declared as `void`/`never` were added to fix some false negatives and keep earlier introduced performance optimization.
    
    GitOrigin-RevId: 28777629a39bd8b4c88c0492707281cff7bf7f6b

--- src/com/jetbrains/php/psalm/PsalmNoReturnProvider.java
+++ src/com/jetbrains/php/psalm/PsalmNoReturnProvider.java
@@ -1,9 +1,11 @@
 package com.jetbrains.php.psalm;
 
 import com.intellij.openapi.util.Ref;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.ResolveResult;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.util.indexing.FileBasedIndex;
+import com.jetbrains.php.lang.PhpLangUtil;
 import com.jetbrains.php.lang.psi.PhpNoReturnProvider;
 import com.jetbrains.php.lang.psi.elements.Function;
 import com.jetbrains.php.lang.psi.elements.FunctionReference;
@@ -14,16 +16,30 @@ import java.util.Arrays;
 public class PsalmNoReturnProvider implements PhpNoReturnProvider {
   @Override
   public boolean isCustomNoReturn(@NotNull FunctionReference functionCall) {
-    return Arrays.stream(functionCall.multiResolve(false))
+    String name = StringUtil.toLowerCase(functionCall.getName());
+    if (StringUtil.isEmpty(name)) return false;
+    return mayBeCustomReturn(functionCall, name) && Arrays.stream(functionCall.multiResolve(false))
       .map(ResolveResult::getElement)
-      .anyMatch(e -> e instanceof Function && containsNoReturnPhpDocType(((Function)e)));
+      .anyMatch(e -> e instanceof Function && containsNoReturnPhpDocType(((Function)e), StringUtil.toLowerCase(((Function)e).getName())));
   }
 
-  private static boolean containsNoReturnPhpDocType(@NotNull Function function) {
+  private static boolean mayBeCustomReturn(@NotNull FunctionReference functionCall, String name) {
     Ref<Boolean> res = new Ref<>(false);
-    FileBasedIndex.getInstance().processValues(PsalmNoReturnFunctionsIndex.KEY, function.getFQN(), null, (file, value) -> {
+    FileBasedIndex.getInstance().processValues(PsalmNoReturnFunctionsIndex.KEY, name, null, (file, value) -> {
       res.set(true);
       return false;
+    }, GlobalSearchScope.allScope(functionCall.getProject()));
+    return res.get();
+  }
+
+  private static boolean containsNoReturnPhpDocType(@NotNull Function function, String name) {
+    Ref<Boolean> res = new Ref<>(false);
+    FileBasedIndex.getInstance().processValues(PsalmNoReturnFunctionsIndex.KEY, name, null, (file, value) -> {
+      if (PhpLangUtil.equalsFunctionNames(function.getFQN(), value)) {
+        res.set(true);
+        return false;
+      }
+      return true;
     }, GlobalSearchScope.allScope(function.getProject()));
     return res.get();
   }
