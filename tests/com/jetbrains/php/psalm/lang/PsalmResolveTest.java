798d3c38aad3d05f44aff366c0f9f586fb804923
Artemy Pestretsov <artemy.pestretsov@jetbrains.com>
2024-10-07 14:12:59 +0200

    WI-74237 groovy -> java leftovers
    GitOrigin-RevId: c7790b4006074b8e147a3fda37d549f4d4ba2063

--- tests/com/jetbrains/php/psalm/lang/PsalmResolveTest.java
+++ tests/com/jetbrains/php/psalm/lang/PsalmResolveTest.java
@@ -1,304 +1,319 @@
-package com.jetbrains.php.psalm.lang
+package com.jetbrains.php.psalm.lang;
 
-import com.intellij.psi.PsiPolyVariantReference
-import com.intellij.util.containers.ContainerUtil
-import com.jetbrains.php.fixtures.PhpCodeInsightFixtureTestCase
-import com.jetbrains.php.lang.psi.elements.Field
-import com.jetbrains.php.lang.psi.elements.Method
-import com.jetbrains.php.lang.psi.elements.PhpClass
-import com.jetbrains.php.psalm.types.PsalmTypeInferenceTest
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiPolyVariantReference;
+import com.intellij.psi.PsiReference;
+import com.intellij.psi.ResolveResult;
+import com.intellij.util.containers.ContainerUtil;
+import com.jetbrains.php.fixtures.PhpCodeInsightFixtureTestCase;
+import com.jetbrains.php.lang.psi.elements.Field;
+import com.jetbrains.php.lang.psi.elements.Method;
+import com.jetbrains.php.lang.psi.elements.PhpClass;
+import com.jetbrains.php.psalm.types.PsalmTypeInferenceTest;
+import org.jetbrains.annotations.NotNull;
 
-class PsalmResolveTest extends PhpCodeInsightFixtureTestCase{
+import java.util.Set;
+
+public class PsalmResolveTest extends PhpCodeInsightFixtureTestCase {
   @Override
-  protected String getTestDataHome() {
-    return PsalmTypeInferenceTest.TEST_DATA_HOME
+  protected @NotNull String getTestDataHome() {
+    return PsalmTypeInferenceTest.TEST_DATA_HOME;
   }
 
   @Override
   protected String getFixtureTestDataFolder() {
-    return "resolve"
+    return "resolve";
   }
 
-  void testExtendedClassConstantReferenceWithWildcard() throws Throwable {
-    configure('''
-<?php
-
-class C{
-    const A_1 = 1;
-    const A_2 = "a";
-    const B_2 = "a";
-}
-
-function f($a, $b)
-{
-    /** @psalm-var C::<caret>A_* $c */
-    $c;
-}
-''')
-    def reference = reference()
-    assertInstanceOf(reference, PsiPolyVariantReference.class)
-    def multiResolve = reference.multiResolve(false)
-    assertSize(2, multiResolve)
-    def first = multiResolve[0].getElement()
-    def second = multiResolve[1].getElement()
-    assertInstanceOf(first, Field.class)
-    assertInstanceOf(second, Field.class)
-    assertEquals(Set.of("\\C.A_1", "\\C.A_2"), Set.of(first.getFQN(), second.getFQN()))
+  public void testExtendedClassConstantReferenceWithWildcard() {
+    configure("""
+                
+                <?php
+                
+                class C{
+                    const A_1 = 1;
+                    const A_2 = "a";
+                    const B_2 = "a";
+                }
+                
+                function f($a, $b)
+                {
+                    /** @psalm-var C::<caret>A_* $c */
+                    $c;
+                }
+                """);
+    PsiReference reference = reference();
+    PsiPolyVariantReference polyVariantReference = assertInstanceOf(reference, PsiPolyVariantReference.class);
+    ResolveResult[] multiResolve = polyVariantReference.multiResolve(false);
+    assertSize(2, multiResolve);
+    Field first = assertInstanceOf(multiResolve[0].getElement(), Field.class);
+    Field second = assertInstanceOf(multiResolve[1].getElement(), Field.class);
+    assertEquals(Set.of("\\C.A_1", "\\C.A_2"), Set.of(first.getFQN(), second.getFQN()));
   }
 
-  void testExtendedClassConstantReferenceWithFullWildcard() throws Throwable {
-    configure('''
-<?php
-
-class C{
-    const A_1 = 1;
-    const A_2 = "a";
-    const B_2 = "a";
-}
-
-function f($a, $b)
-{
-    /** @psalm-var C::<caret>* $c */
-    $c;
-}
-''')
-    def reference = reference()
-    assertInstanceOf(reference, PsiPolyVariantReference.class)
-    def multiResolve = reference.multiResolve(false)
-    assertSize(3, multiResolve)
-    def fqns = ContainerUtil.map2Set(multiResolve, { assertInstanceOf(it.getElement(), Field.class).getFQN() })
-    assertEquals(Set.of("\\C.A_1", "\\C.A_2", "\\C.B_2"), fqns)
+  public void testExtendedClassConstantReferenceWithFullWildcard() {
+    configure("""
+                
+                <?php
+                
+                class C{
+                    const A_1 = 1;
+                    const A_2 = "a";
+                    const B_2 = "a";
+                }
+                
+                function f($a, $b)
+                {
+                    /** @psalm-var C::<caret>* $c */
+                    $c;
+                }
+                """);
+    PsiReference reference = reference();
+    PsiPolyVariantReference polyVariantReference = assertInstanceOf(reference, PsiPolyVariantReference.class);
+    ResolveResult[] multiResolve = polyVariantReference.multiResolve(false);
+    assertSize(3, multiResolve);
+    Set<String> fqns = ContainerUtil.map2Set(multiResolve, it -> {
+      return assertInstanceOf(it.getElement(), Field.class).getFQN();
+    });
+    assertEquals(Set.of("\\C.A_1", "\\C.A_2", "\\C.B_2"), fqns);
   }
 
-
-  void testResolveAliasReferenceToType() throws Throwable {
-    configure('''
-<?php
-/**
- * @psalm-type A_OR_B = A|B
- * @psalm-type CoolType = <caret>A_OR_B|null
- * @return CoolType
- */
-function foo($a) {
-  
-}
-''')
-    def resolved = resolve()
-    assertEquals("A_OR_B", resolved.getText())
-    assertEquals("@psalm-type A_OR_B = A|B", resolved.getParent().getText())
+  public void testResolveAliasReferenceToType() {
+    configure("""
+                
+                <?php
+                /**
+                 * @psalm-type A_OR_B = A|B
+                 * @psalm-type CoolType = <caret>A_OR_B|null
+                 * @return CoolType
+                 */
+                function foo($a) {
+                 \s
+                }
+                """);
+    PsiElement resolved = resolve();
+    assertEquals("A_OR_B", resolved.getText());
+    assertEquals("@psalm-type A_OR_B = A|B", resolved.getParent().getText());
   }
 
-  void testResolveImportedAliasReferenceToType() throws Throwable {
-    configure('''
-<?php
-/**
- * @psalm-import-type FooAlias from Phone as MyFooAlias
- * @psalm-type CoolType = <caret>MyFooAlias|null
- */
-function foo($a) {
-  
-}
-''')
-    def resolved = resolve()
-    assertEquals("MyFooAlias", resolved.getText())
-    assertEquals("@psalm-import-type FooAlias from Phone as MyFooAlias", resolved.getParent().getText())
+  public void testResolveImportedAliasReferenceToType() {
+    configure("""
+                
+                <?php
+                /**
+                 * @psalm-import-type FooAlias from Phone as MyFooAlias
+                 * @psalm-type CoolType = <caret>MyFooAlias|null
+                 */
+                function foo($a) {
+                 \s
+                }
+                """);
+    PsiElement resolved = resolve();
+    assertEquals("MyFooAlias", resolved.getText());
+    assertEquals("@psalm-import-type FooAlias from Phone as MyFooAlias", resolved.getParent().getText());
   }
 
-  void testResolveImportedAliasFromMultilineReferenceToType() throws Throwable {
-    configure('''
-<?php
-/**
- * @psalm-import-type FooAlias from Phone as MyFooAlias
- * @psalm-type FileMapType = array{
- *      0: <caret>MyFooAlias
- * }
- */
-function foo($a) {
-  
-}
-''')
-    def resolved = resolve()
-    assertEquals("MyFooAlias", resolved.getText())
-    assertEquals("@psalm-import-type FooAlias from Phone as MyFooAlias", resolved.getParent().getText())
+  public void testResolveImportedAliasFromMultilineReferenceToType() {
+    configure("""
+                
+                <?php
+                /**
+                 * @psalm-import-type FooAlias from Phone as MyFooAlias
+                 * @psalm-type FileMapType = array{
+                 *      0: <caret>MyFooAlias
+                 * }
+                 */
+                function foo($a) {
+                 \s
+                }
+                """);
+    PsiElement resolved = resolve();
+    assertEquals("MyFooAlias", resolved.getText());
+    assertEquals("@psalm-import-type FooAlias from Phone as MyFooAlias", resolved.getParent().getText());
   }
 
-  void testResolveImportedAliasFromPluralReferenceToType() throws Throwable {
-    configure('''
-<?php
-/**
- * @psalm-import-type FooAlias from Phone as MyFooAlias
- * @psalm-type FileMapType = <caret>MyFooAlias[]
- */
-function foo($a) {
-  
-}
-''')
-    def resolved = resolve()
-    assertEquals("MyFooAlias", resolved.getText())
-    assertEquals("@psalm-import-type FooAlias from Phone as MyFooAlias", resolved.getParent().getText())
+  public void testResolveImportedAliasFromPluralReferenceToType() {
+    configure("""
+                
+                <?php
+                /**
+                 * @psalm-import-type FooAlias from Phone as MyFooAlias
+                 * @psalm-type FileMapType = <caret>MyFooAlias[]
+                 */
+                function foo($a) {
+                 \s
+                }
+                """);
+    PsiElement resolved = resolve();
+    assertEquals("MyFooAlias", resolved.getText());
+    assertEquals("@psalm-import-type FooAlias from Phone as MyFooAlias", resolved.getParent().getText());
   }
 
-
-
-  void testResolveImportedFromStatement() throws Throwable {
-    configure('''
-<?php
-/**
- * @psalm-import-type FooAlias from Phone as MyFooAlias
- */
-function foo($a) {
-    /** @var <caret>MyFooAlias $alias */
-    $phone = $param->toArray();
-}
-''')
-    def resolved = resolve()
-    assertEquals("MyFooAlias", resolved.getText())
-    assertEquals("@psalm-import-type FooAlias from Phone as MyFooAlias", resolved.getParent().getText())
+  public void testResolveImportedFromStatement() {
+    configure("""
+                
+                <?php
+                /**
+                 * @psalm-import-type FooAlias from Phone as MyFooAlias
+                 */
+                function foo($a) {
+                    /** @var <caret>MyFooAlias $alias */
+                    $phone = $param->toArray();
+                }
+                """);
+    PsiElement resolved = resolve();
+    assertEquals("MyFooAlias", resolved.getText());
+    assertEquals("@psalm-import-type FooAlias from Phone as MyFooAlias", resolved.getParent().getText());
   }
 
-  void testResolveImportedFromStatementToContainingClass() throws Throwable {
-    configure('''
-<?php
-/**
- * @psalm-import-type FooAlias from Phone as MyFooAlias
- */
-class a {
-function foo($a) {
-    /** @var <caret>MyFooAlias $alias */
-    $phone = $param->toArray();
-}
-}
-''')
-    def resolved = resolve()
-    assertEquals("MyFooAlias", resolved.getText())
-    assertEquals("@psalm-import-type FooAlias from Phone as MyFooAlias", resolved.getParent().getText())
+  public void testResolveImportedFromStatementToContainingClass() {
+    configure("""
+                
+                <?php
+                /**
+                 * @psalm-import-type FooAlias from Phone as MyFooAlias
+                 */
+                class a {
+                function foo($a) {
+                    /** @var <caret>MyFooAlias $alias */
+                    $phone = $param->toArray();
+                }
+                }
+                """);
+    PsiElement resolved = resolve();
+    assertEquals("MyFooAlias", resolved.getText());
+    assertEquals("@psalm-import-type FooAlias from Phone as MyFooAlias", resolved.getParent().getText());
   }
 
-  void testResolveAliasToReferenceTypeFromClosure() throws Throwable {
-    configure('''
-<?php
-/**
- * @psalm-import-type FooAlias from Phone as MyFooAlias
- */
-class a {
-function foo($a) {
-        /**
-         * @psalm-return <caret>MyFooAlias
-         */
-        $param = function () {};
-}
-}
-''')
-    def resolved = resolve()
-    assertEquals("MyFooAlias", resolved.getText())
-    assertEquals("@psalm-import-type FooAlias from Phone as MyFooAlias", resolved.getParent().getText())
+  public void testResolveAliasToReferenceTypeFromClosure() {
+    configure("""
+                
+                <?php
+                /**
+                 * @psalm-import-type FooAlias from Phone as MyFooAlias
+                 */
+                class a {
+                function foo($a) {
+                        /**
+                         * @psalm-return <caret>MyFooAlias
+                         */
+                        $param = function () {};
+                }
+                }
+                """);
+    PsiElement resolved = resolve();
+    assertEquals("MyFooAlias", resolved.getText());
+    assertEquals("@psalm-import-type FooAlias from Phone as MyFooAlias", resolved.getParent().getText());
   }
 
-
-  void testResolveImportedFromMemberToContainingClass() throws Throwable {
-    configure('''
-<?php
-/**
- * @psalm-import-type FooAlias from Phone as MyFooAlias
- */
-class a {
-/**
- * @param $param
- * @return <caret>MyFooAlias
- */
-function foo($a) {}
-}
-''')
-    def resolved = resolve()
-    assertEquals("MyFooAlias", resolved.getText())
-    assertEquals("@psalm-import-type FooAlias from Phone as MyFooAlias", resolved.getParent().getText())
+  public void testResolveImportedFromMemberToContainingClass() {
+    configure("""
+                
+                <?php
+                /**
+                 * @psalm-import-type FooAlias from Phone as MyFooAlias
+                 */
+                class a {
+                /**
+                 * @param $param
+                 * @return <caret>MyFooAlias
+                 */
+                function foo($a) {}
+                }
+                """);
+    PsiElement resolved = resolve();
+    assertEquals("MyFooAlias", resolved.getText());
+    assertEquals("@psalm-import-type FooAlias from Phone as MyFooAlias", resolved.getParent().getText());
   }
 
-  void testResolveImportedFromClass() throws Throwable {
-    configure('''
-<?php
-/**
- * @psalm-import-type FooAlias from Phone as MyFooAlias
- * @psalm-type TestType = array{base: <caret>MyFooAlias, description: string}  
- */
-class a {
-function foo($a) {
-}
-''')
-    def resolved = resolve()
-    assertEquals("MyFooAlias", resolved.getText())
-    assertEquals("@psalm-import-type FooAlias from Phone as MyFooAlias", resolved.getParent().getText())
+  public void testResolveImportedFromClass() {
+    configure("""
+                
+                <?php
+                /**
+                 * @psalm-import-type FooAlias from Phone as MyFooAlias
+                 * @psalm-type TestType = array{base: <caret>MyFooAlias, description: string} \s
+                 */
+                class a {
+                function foo($a) {
+                }
+                """);
+    PsiElement resolved = resolve();
+    assertEquals("MyFooAlias", resolved.getText());
+    assertEquals("@psalm-import-type FooAlias from Phone as MyFooAlias", resolved.getParent().getText());
   }
 
-  void "test find constant by special self class name in @see tag"() {
-    configure('''
-<?php
+  public void test_find_constant_by_special_self_class_name_in__see_tag() {
+    configure("""
+                
+                <?php
+                
+                namespace {
+                    /**
+                     * @see <caret>self::foo()
+                     */
+                    class C {
+                       function foo();
+                    }
+                }""");
 
-namespace {
-    /**
-     * @see <caret>self::foo()
-     */
-    class C {
-       function foo();
-    }
-}''')
-
-    def phpClass = assertInstanceOf(resolve(), PhpClass.class)
-    assertEquals "\\C", phpClass.getFQN()
+    PhpClass phpClass = assertInstanceOf(resolve(), PhpClass.class);
+    assertEquals("\\C", phpClass.getFQN());
   }
 
-  void "test find constant by special parent class name in @see tag"() {
-    configure('''
-<?php
+  public void test_find_constant_by_special_parent_class_name_in__see_tag() {
+    configure("""
+                
+                <?php
+                
+                namespace {
+                
+                    class B {}
+                    /**
+                     * @see <caret>parent::foo()
+                     */
+                    class C extends B {
+                       function foo();
+                    }
+                }""");
 
-namespace {
-
-    class B {}
-    /**
-     * @see <caret>parent::foo()
-     */
-    class C extends B {
-       function foo();
-    }
-}''')
-
-    def phpClass = assertInstanceOf(resolve(), PhpClass.class)
-    assertEquals "\\B", phpClass.getFQN()
+    PhpClass phpClass = assertInstanceOf(resolve(), PhpClass.class);
+    assertEquals("\\B", phpClass.getFQN());
   }
 
-  void testGenericMixinMethod() {
-    configureByFile()
-    def method = assertInstanceOf(resolve(), Method.class)
-    assertEquals "\\MixinClass.mixinClassMethod", method.getFQN()
+  public void testGenericMixinMethod() {
+    configureByFile();
+    Method method = assertInstanceOf(resolve(), Method.class);
+    assertEquals("\\MixinClass.mixinClassMethod", method.getFQN());
   }
 
-  void testGenericMixinField() {
-    configureByFile()
-    def field = assertInstanceOf(resolve(), Field.class)
-    assertEquals "\\MixinClass.\$mixinClassProperty", field.getFQN()
+  public void testGenericMixinField() {
+    configureByFile();
+    Field field = assertInstanceOf(resolve(), Field.class);
+    assertEquals("\\MixinClass.$mixinClassProperty", field.getFQN());
   }
 
-  void testGenericMixinDecorator() {
-    configureByFile()
-    def method = assertInstanceOf(resolve(), Method.class)
-    assertEquals "\\ApiClient.request", method.getFQN()
+  public void testGenericMixinDecorator() {
+    configureByFile();
+    Method method = assertInstanceOf(resolve(), Method.class);
+    assertEquals("\\ApiClient.request", method.getFQN());
   }
 
-  void testGenericMixinSeveralMixins() {
-    configureByFile()
-    def method = assertInstanceOf(resolve(), Method.class)
-    assertEquals "\\MixinClass2.mixinClassMethod2", method.getFQN()
+  public void testGenericMixinSeveralMixins() {
+    configureByFile();
+    Method method = assertInstanceOf(resolve(), Method.class);
+    assertEquals("\\MixinClass2.mixinClassMethod2", method.getFQN());
   }
 
-  void testGenericMixinUnionTwoClassesWithMixins() {
-    configureByFile()
-    def method = assertInstanceOf(resolve(), Method.class)
-    assertEquals "\\MixinClass2.mixinClassMethod2", method.getFQN()
+  public void testGenericMixinUnionTwoClassesWithMixins() {
+    configureByFile();
+    Method method = assertInstanceOf(resolve(), Method.class);
+    assertEquals("\\MixinClass2.mixinClassMethod2", method.getFQN());
   }
 
-  void testGenericMixinGenericAndPlainMixins() {
-    configureByFile()
-    def method = assertInstanceOf(resolve(), Method.class)
-    assertEquals "\\MixinClass2.mixinClassMethod2", method.getFQN()
+  public void testGenericMixinGenericAndPlainMixins() {
+    configureByFile();
+    Method method = assertInstanceOf(resolve(), Method.class);
+    assertEquals("\\MixinClass2.mixinClassMethod2", method.getFQN());
   }
 }
--- tests/com/jetbrains/php/psalm/quality/tools/PsalmConfigFileFromComposerTest.java
+++ tests/com/jetbrains/php/psalm/quality/tools/PsalmConfigFileFromComposerTest.java
@@ -1,79 +1,77 @@
-package com.jetbrains.php.psalm.quality.tools
+package com.jetbrains.php.psalm.quality.tools;
 
-import com.intellij.testFramework.fixtures.CodeInsightFixtureTestCase
-import com.jetbrains.php.tools.quality.psalm.PsalmComposerConfig
-import org.jetbrains.annotations.NotNull
-import org.jetbrains.annotations.Nullable 
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.testFramework.fixtures.CodeInsightFixtureTestCase;
+import com.jetbrains.php.tools.quality.psalm.PsalmComposerConfig;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-class PsalmConfigFileFromComposerTest extends CodeInsightFixtureTestCase {
-
-  @NotNull
+public class PsalmConfigFileFromComposerTest extends CodeInsightFixtureTestCase {
   private void doRulesetTest(@NotNull String content, @Nullable String expectedPath) {
-    myFixture.configureByText("composer.json", content)
-    try {
-      def configFile = myFixture.getFile().getVirtualFile()
-      assertEquals(expectedPath, new PsalmComposerConfig().getRuleset(configFile))
-    }
-    finally {
-    }
+    myFixture.configureByText("composer.json", content);
+    VirtualFile configFile = myFixture.getFile().getVirtualFile();
+    assertEquals(expectedPath, new PsalmComposerConfig().getRuleset(configFile));
   }
 
-  void testCFormat() {
-    doRulesetTest('''
-{
-    "scripts": {
-        "psalm": "./vendor/bin/psalm -c src/psalm.xml"
-    }
-}
-''', "src/psalm.xml")
+  public void testCFormat() {
+    doRulesetTest("""
+                    
+                    {
+                        "scripts": {
+                            "psalm": "./vendor/bin/psalm -c src/psalm.xml"
+                        }
+                    }
+                    """, "src/psalm.xml");
   }
 
-  void testNoScriptsSection() {
-    doRulesetTest('''
-{
-}
-''', null)
+  public void testNoScriptsSection() {
+    doRulesetTest("""
+                    
+                    {
+                    }
+                    """, null);
   }
 
-  void testNoPsalmInScriptsSection() {
-    doRulesetTest('''
-{
-    "scripts": {
-    }
-}
-''', null)
-  }
-  
-
-  void testConfigFormat() {
-    doRulesetTest('''
-{
-  "scripts": {
-        "psalm": "./vendor/bin/psalm --config=src/psalm.xml"
-  }
-}
-''', "src/psalm.xml")
+  public void testNoPsalmInScriptsSection() {
+    doRulesetTest("""
+                    
+                    {
+                        "scripts": {
+                        }
+                    }
+                    """, null);
   }
 
-  void testNoConfigArg() {
-    doRulesetTest('''
-{
-  "scripts": {
-        "psalm": "@php ./vendor/bin/psalm --init"
-  }
-}
-''', null)
-  }
-
-
-  void testWrongFormat() {
-    doRulesetTest('''
-{
-"scripts": {
-        "psalm": "./vendor/bin/psalm -c"
-  }
-}
-''', null)
+  public void testConfigFormat() {
+    doRulesetTest("""
+                    
+                    {
+                      "scripts": {
+                            "psalm": "./vendor/bin/psalm --config=src/psalm.xml"
+                      }
+                    }
+                    """, "src/psalm.xml");
   }
 
+  public void testNoConfigArg() {
+    doRulesetTest("""
+                    
+                    {
+                      "scripts": {
+                            "psalm": "@php ./vendor/bin/psalm --init"
+                      }
+                    }
+                    """, null);
+  }
+
+  public void testWrongFormat() {
+    doRulesetTest("""
+                    
+                    {
+                    "scripts": {
+                            "psalm": "./vendor/bin/psalm -c"
+                      }
+                    }
+                    """, null);
+  }
 }
