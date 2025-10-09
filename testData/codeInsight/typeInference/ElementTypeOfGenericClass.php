867e141c50d3ca5b13f745b5feac2d008dfb1a2a
Florian Schaefer <florian.schaefer@jetbrains.com>
2025-10-09 13:56:06 +0200

    [WI-76605-addendum]: Add support for previously overlooked recursive generics use cases
    (cherry picked from commit c07c4e11e285d7bf1764e52dc3a5cdf3fb89b480)
    
    IJ-MR-178068
    
    GitOrigin-RevId: e7c2644292fb10943fd5d1d78a27b8082d55ea85

--- testData/codeInsight/typeInference/ElementTypeOfGenericClass.php
+++ testData/codeInsight/typeInference/ElementTypeOfGenericClass.php
@@ -32,5 +32,5 @@ class A {
  * @param A<int, Exception> $param
  */
 function f2($param){
-    <type value="Exception|mixed">$param->lazy()->all()[0]</type>;
+    <type value="int|Exception|mixed">$param->lazy()->all()[0]</type>;
 }
